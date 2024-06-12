a8ba80a4a46007654ba3940e1ea68150a07ad7d0
7byLoper <7byloper@gmail.com>
2024-06-12 15:20:26 +0300

    Add files via upload

---
+++ src/main/java/by/loper/SunPickaxe3x3/PickaxeManager.java
@@ -0,0 +1,76 @@
+package by.loper.SunPickaxe3x3;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PickaxeManager {
+    private static ItemStack pickaxe = new ItemStack(Material.NETHERITE_PICKAXE);
+    private static String PickaxeDisplayName = SunPickaxe3x3.getInstance().getConfig().getString("Pickaxe.DisplayName").replace("&","§");
+    private static List<String> lore = SunPickaxe3x3.getInstance().getConfig().getStringList("Pickaxe.Lore");
+    private static ItemMeta meta;
+    public static void givePickaxe(Player player) {
+        meta = pickaxe.getItemMeta();
+        if(lore != null){
+            meta.setLore(lore);
+        }
+        if(PickaxeDisplayName != null){
+            meta.setDisplayName(PickaxeDisplayName);
+        }
+        meta.getPersistentDataContainer().set(new NamespacedKey(SunPickaxe3x3.getInstance(), "sunPickaxe"), PersistentDataType.STRING, "3x3Pickaxe");
+        pickaxe.setItemMeta(meta);
+        player.getInventory().addItem(pickaxe);
+    }
+    public static List<Block> select(Location loc1,Location loc2){
+        List<Block> blocks = new ArrayList<Block>();
+        int x1 = loc1.getBlockX();
+        int y1 = loc1.getBlockY();
+        int z1 = loc1.getBlockZ();
+        int x2 = loc2.getBlockX();
+        int y2 = loc2.getBlockY();
+        int z2 = loc2.getBlockZ();
+        int xMin, yMin, zMin;
+        int xMax, yMax, zMax;
+        int x, y, z;
+        if(x1 > x2){
+            xMin = x2;
+            xMax = x1;
+        }else{
+            xMin = x1;
+            xMax = x2;
+        }
+        if(y1 > y2){
+            yMin = y2;
+            yMax = y1;
+        }else{
+            yMin = y1;
+            yMax = y2;
+        }
+        if(z1 > z2){
+            zMin = z2;
+            zMax = z1;
+        }else{
+            zMin = z1;
+            zMax = z2;
+        }
+        for(x = xMin; x <= xMax; x ++){
+            for(y = yMin; y <= yMax; y ++){
+                for(z = zMin; z <= zMax; z ++){
+                    Block b = new Location(loc1.getWorld(), x, y, z).getBlock();
+                    blocks.add(b);
+                }
+            }
+        }
+        return blocks;
+    }
+}
